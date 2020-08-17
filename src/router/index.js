f7ff7fb4705324bfb450d03641bbe96dd87881e1
J Kaufman <jtkaufman737@gmail.com>
2020-08-17 19:25:14 -0400

    reintroduce all routes and router links

--- src/router/index.js
+++ src/router/index.js
@@ -11,97 +11,97 @@ Vue.use(VueRouter)
       return import('../views/Home.vue')
     }
   },
-  // {
-  //   path: '/WeatherHome',
-  //   name: 'WeatherHome',
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ '../views/WeatherHome.vue')
-  //   }
-  // },
-  // {
-  //   path: '/WeatherSecure',
-  //   name: 'Weather',
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ '../views/WeatherSecure.vue')
-  //   }
-  // },
-  // {
-  //   path: '/login',
-  //   name: 'Login',
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ '../views/LoginSecure.vue')
-  //   }
-  // },
-  // {
-  //   path: '/logini',
-  //   name: 'LoginInsecure',
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ '../views/LoginInsecure.vue')
-  //   }
-  // },
-  // {
-  //   path: '/fileDownloadHome',
-  //   name: 'LoginInsecure',
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ '../views/FileDownloadHome.vue')
-  //   }
-  // },
-  // {
-  //   path: '/fileDownloadi',
-  //   name: 'LoginInsecure',
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ '../views/FileDownloadInsecure.vue')
-  //   }
-  // },
-  // {
-  //   path: '/fileDownload',
-  //   name: 'FileDownload',
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ '../views/FileDownloadSecure.vue')
-  //   }
-  // },
-  // {
-  //   path: '/hardcode',
-  //   name: 'HardcodeHome',
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ '../views/HardcodedCredsHome.vue')
-  //   }
-  // },
-  // {
-  //   path: '/hardcodei',
-  //   name: 'HardcodeCredsInsecure',
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ '../views/HardcodedCredsInsecure.vue')
-  //   }
-  // },
-  // {
-  //   path: '/hardcodes',
-  //   name: 'HardcodeCredsSecure',
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ '../views/HardcodedCredsSecure.vue')
-  //   }
-  // },
-  // {
-  //   path: '/encrypt',
-  //   name: 'MissingEncryptionHome',
-  //   component: function() {
-  //     return import(/* webpackChunkName: "about" */ '../views/MissingEncryptionHome.vue')
-  //   }
-  // },
-  // {
-  //   path: '/encrypti',
-  //   name: 'MissingEncryptionInsecure',
-  //   component: function() {
-  //     return import(/* webpackChunkName: "about" */ '../views/MissingEncryptionInsecure.vue')
-  //   }
-  // },
-  // {
-  //   path: '/encrypts',
-  //   name: 'MissingEncryptionSecure',
-  //   component: function() {
-  //     return import(/* webpackChunkName: "about" */ '../views/MissingEncryptionSecure.vue')
-  //   }
-  // }
+  {
+    path: '/WeatherHome',
+    name: 'WeatherHome',
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/WeatherHome.vue')
+    }
+  },
+  {
+    path: '/WeatherSecure',
+    name: 'Weather',
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/WeatherSecure.vue')
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/LoginSecure.vue')
+    }
+  },
+  {
+    path: '/logini',
+    name: 'LoginInsecure',
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/LoginInsecure.vue')
+    }
+  },
+  {
+    path: '/fileDownloadHome',
+    name: 'LoginInsecure',
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/FileDownloadHome.vue')
+    }
+  },
+  {
+    path: '/fileDownloadi',
+    name: 'LoginInsecure',
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/FileDownloadInsecure.vue')
+    }
+  },
+  {
+    path: '/fileDownload',
+    name: 'FileDownload',
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/FileDownloadSecure.vue')
+    }
+  },
+  {
+    path: '/hardcode',
+    name: 'HardcodeHome',
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/HardcodedCredsHome.vue')
+    }
+  },
+  {
+    path: '/hardcodei',
+    name: 'HardcodeCredsInsecure',
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/HardcodedCredsInsecure.vue')
+    }
+  },
+  {
+    path: '/hardcodes',
+    name: 'HardcodeCredsSecure',
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/HardcodedCredsSecure.vue')
+    }
+  },
+  {
+    path: '/encrypt',
+    name: 'MissingEncryptionHome',
+    component: function() {
+      return import(/* webpackChunkName: "about" */ '../views/MissingEncryptionHome.vue')
+    }
+  },
+  {
+    path: '/encrypti',
+    name: 'MissingEncryptionInsecure',
+    component: function() {
+      return import(/* webpackChunkName: "about" */ '../views/MissingEncryptionInsecure.vue')
+    }
+  },
+  {
+    path: '/encrypts',
+    name: 'MissingEncryptionSecure',
+    component: function() {
+      return import(/* webpackChunkName: "about" */ '../views/MissingEncryptionSecure.vue')
+    }
+  },
   {
     path: '/hash',
     name: 'HashHome',
